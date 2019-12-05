ff008a7920fc43d09f6ed0c3d066781e013d3b9c
sagolwong <5910110678@psu.ac.th>
2019-12-05 15:59:07 +0700

    fix state of loading

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -11,15 +11,17 @@ import {FIREBASE_CONFIG, snapshotToArray} from '../config/firebaseconfig';
 export class Tab4Page implements OnInit {
 
   items = [];
+  num:number = 0;
   ref = firebase.database().ref('posts/')
   constructor(private router: Router) { 
     this.ref.on('value', res =>{
       this.items = snapshotToArray(res).filter((item)=>{
         if(item.userId === 'user001'){
+          this.num = 1;
           return item;
         }
+        this.num = 2;
       });
-      
     })
   }
 
